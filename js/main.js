69ced2c5e96dfaf4cf6e0b84834e20fcd213e679
ewoksic <jasicn@gmail.com>
2019-05-07 00:18:17 +0200

    sitne izmene

--- js/main.js
+++ js/main.js
@@ -8,14 +8,4 @@ function myFunction() {
     }
   } 
 
-// slider index page
 
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
-
-function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
